8c02ab2cb42080abd7342691bd5d43fd9736d8f5
Fulvio <fulvio.manfrin@lighthouseit.com.br>
2024-04-25 23:08:28 -0300

    ADD implementacao

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+var nokia = new Nokia("988556668", "Nokia1", "7777888999554", 16);
+var iphone = new Iphone("988886668", "Iphone 13", "7777888669554", 16);
+
+Console.WriteLine("Smartphone Nokia:");
+
+nokia.InstalarAplicativo("WhatsApp");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+Console.WriteLine("----------------------------------------------");
+
+Console.WriteLine("Smartphone Iphone:");
+iphone.InstalarAplicativo("Telegram");
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+Console.WriteLine("----------------------------------------------");
